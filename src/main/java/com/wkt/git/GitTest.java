7c074bcea4af802be4e035b79a4840e6b8d5718f
shuixingge-001 <827583430@qq.com>
2021-10-28 09:31:48 +0800

    second commit

--- src/main/java/com/wkt/git/GitTest.java
+++ src/main/java/com/wkt/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("测试中.....");
         System.out.println("测试中1....");
+        System.out.println("测试中2....");
     }
 }
